085d7094faf94571c0421ca460520fe661ec3fb9
yanggh02 <8133259@gmail.com>
2021-06-13 21:14:42 +0900

    Update greetting.js

--- greetting.js
+++ greetting.js
@@ -1,5 +1,5 @@
 const form = document.querySelector(".js-form"),
-input = form.querySelector("input");
+input = form.querySelector("input"),
 greet = document.querySelector(".js-greets");
 
 const USER_LS = "curUser",
@@ -14,18 +14,34 @@ function eventSub(event){
     const curVal = input.value;
     paintGreet(curVal);
     saveName(curVal);
+    input.value = "";
 }
 
 function askName(){
+    greet.classList.remove(SHOWING_CN);
     form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", eventSub);
 }
 
 function paintGreet(text){
     form.classList.remove(SHOWING_CN);
     greet.classList.add(SHOWING_CN);
-    greet.innerText = `안녕하세요 ${text}.`;
+    hourlyGreets(text);
+}
 
+function hourlyGreets(text) {
+    const date = new Date(),
+    time = date.getHours();
+    if(6 < time && time < 12) {
+        greet.innerText = `좋은 아침입니다, ${text}.`;
+    } else if(11 < time && time < 18) {
+        greet.innerText = `좋은 오후입니다, ${text}.`;
+    } else if(17 < time && time < 21) {
+        greet.innerText = `좋은 저녁입니다, ${text}.`;
+    } else if(20 < time || time === 0) {
+        greet.innerText = `좋은 밤입니다, ${text}.`;
+    } else {
+        greet.innerText = `좋은 새벽입니다, ${text}.`;
+    }
 }
 
 function loadName(){
@@ -39,6 +55,8 @@ function loadName(){
 
 function init(){
     loadName();
+    setInterval(loadName, 1000);
+    form.addEventListener("submit", eventSub);
 }
 
 init();
